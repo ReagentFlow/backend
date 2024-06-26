06798fadd2d2e696e0bf452a74a98c39f9ba81d1
ulurn-av <ulurn@yandex.ru>
2024-06-27 00:12:56 +0700

    User model and bugs

--- authentication/api/urls.py
+++ authentication/api/urls.py
@@ -1,10 +1,6 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView,
-)
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 router = DefaultRouter()
 
--- authentication/models.py
+++ authentication/models.py
@@ -1,7 +1,46 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager, make_password
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
-# Create your models here.
+class MyUserManager(UserManager):
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("Указанный адрес электронной почты должен быть установлен")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+        return self._create_user(email, password, **extra_fields)
+
 
 class User(AbstractUser):
-    pass
+    email = models.EmailField(_("email address"), unique=True)
+    middle_name = models.CharField(_("middle name"), max_length=150, blank=True)
+    username = None
+
+    objects = MyUserManager()
+
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["last_name", "first_name"]
+
+    def __str__(self):
+        return f"{self.last_name} {self.first_name} {self.middle_name}"
+
+    class Meta(AbstractUser.Meta):
+        ordering = ("last_name", "first_name")
+        verbose_name = _("user")
+        verbose_name_plural = _("users")
+        swappable = "AUTH_USER_MODEL"
