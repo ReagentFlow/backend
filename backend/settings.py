33f4036e5c3126140f8c1b606805f269bb400df2
ulurn-av <ulurn@yandex.ru>
2024-07-01 13:47:43 +0700

    update CORS

--- backend/settings.py
+++ backend/settings.py
@@ -166,10 +166,6 @@ USE_TZ = True
 
 AUTH_USER_MODEL = 'authentication.User'
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
-
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
