3fc601bb66f456e62af66cfe247f555f4e509207
ulurn-av <ulurn@yandex.ru>
2024-07-05 21:53:48 +0700

    bug with permission

--- authentication/api/permissions.py
+++ authentication/api/permissions.py
@@ -1,12 +1,10 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
-from authentication.models import User
-
 
 class IsAuthenticatedAndAdmin(BasePermission):
     def has_permission(self, request, view):
         print(request.user.role)
-        return bool(request.user and request.user.is_authenticated and request.user.role == User.ROLES["admin"])
+        return bool(request.user and request.user.is_authenticated and request.user.role == "admin")
 
 
 class IsAdminOrReadOnly(BasePermission):
@@ -15,5 +13,5 @@ class IsAdminOrReadOnly(BasePermission):
             request.method in SAFE_METHODS or
             request.user and
             request.user.is_authenticated and
-            request.user.role == User.ROLES["admin"]
+            request.user.role == "admin"
         )
