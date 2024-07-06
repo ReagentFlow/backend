9ce6079acacf6e148407f3c92bd4fce75df04bfc
ulurn-av <ulurn@yandex.ru>
2024-07-07 00:03:48 +0700

    FIXED Permissions

--- authentication/api/permissions.py
+++ authentication/api/permissions.py
@@ -1,17 +1,19 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
+from authentication.models import User
 
 
 class IsAuthenticatedAndAdmin(BasePermission):
     def has_permission(self, request, view):
-        print(request.user.role)
-        return bool(request.user and request.user.is_authenticated and request.user.role == "admin")
+        role = request.user.role
+        return bool(request.user and request.user.is_authenticated and role == User.ADMIN)
 
 
 class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
+        role = request.user.role
         return bool(
             request.method in SAFE_METHODS or
             request.user and
             request.user.is_authenticated and
-            request.user.role == "admin"
+            role == User.ADMIN
         )
