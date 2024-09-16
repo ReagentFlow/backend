22460d8734a266f6b7c5cdb8d6347e7da33636c2
ulurn-av <ulurn@yandex.ru>
2024-09-16 21:56:47 +0700

    swagger

--- backend/settings.py
+++ backend/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "djoser",
+    'drf_yasg',
     "rest_framework_simplejwt",
     "corsheaders",
     'django_filters',
@@ -180,7 +181,9 @@ CORS_ALLOW_CREDENTIALS = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
