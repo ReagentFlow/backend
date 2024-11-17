7d1dbaa2ac7a2397e05c493e116031274b1c5ae7
ulurn <ulurn@yandex.ru>
2024-11-17 15:22:18 +0700

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -35,8 +35,8 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("auth/", include(("authentication.api.urls", "authentication"), "auth")),
-    path("data/", include(("reagents.api.urls", "reagents"), "data")),
-    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    path("api/admin/", admin.site.urls),
+    path("api/auth/", include(("authentication.api.urls", "authentication"), "auth")),
+    path("api/data/", include(("reagents.api.urls", "reagents"), "data")),
+    path("api/swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
 ]
